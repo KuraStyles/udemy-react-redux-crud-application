1b3f2de18eba64cefb4a17f15d2c53ba60bba719
KAZUMA KURAMOTO <kazumakuramoto@KAZUMAnoMacBook-puro.local>
2021-01-21 17:24:59 -0800

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const App = () => {
 	const profiles = [
 		{
 			name: "Taro",
-			age: "10",
+			age: 10,
 		},
 		{
 			name: "Hanako",
-			age: "5",
+			age: 5,
 		},
 		{
 			name: "NoName",
+			age: 3,
 		},
 	];
 	return (
@@ -31,7 +33,8 @@ const User = (props) => {
 	);
 };
 
-User.defaultProps = {
-	age: 1,
+User.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number.isRequired,
 };
 export default App;

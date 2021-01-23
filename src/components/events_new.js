5cd541d9d0462c5617798bc49e74028057496a5f
KAZUMA KURAMOTO <kazumakuramoto@KAZUMAnoMacBook-puro.local>
2021-01-22 20:46:03 -0800

    update

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -29,7 +29,7 @@ class EventsNew extends Component {
 		this.props.history.push("/");
 	}
 	render() {
-		const { handleSubmit } = this.props;
+		const { handleSubmit, pristine, submitting } = this.props;
 
 		return (
 			<form onSubmit={handleSubmit(this.onSubmit)}>
@@ -50,7 +50,11 @@ class EventsNew extends Component {
 					/>
 				</div>
 				<div>
-					<input type="submit" value="Submit" disabled={false} />
+					<input
+						type="submit"
+						value="Submit"
+						disabled={pristine || submitting}
+					/>
 					<Link to="/">Cancel</Link>
 				</div>
 			</form>
